6ed86d7b7347378aa5c54aff3ce2982e7cc82db6
Shukhfayz <shukhfayz@gmail.com>
2024-02-24 23:22:49 -0500

    feat: commit H-TASK

---
+++ src/train.ts
@@ -0,0 +1,23 @@
+/*H-TASK: 
+
+Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+MASALAN: gerReverse("hello") return qiladi "olleh"
+
+@MITASK
+*/
+
+//with for loop
+// function gerReverse(str: string) {
+//   for(let i=str.length-1; i>=0; i--) {
+//     console.log(str[i])
+//   }
+// }
+
+// gerReverse("hello")
+
+//with reverse method
+// function gerReverse(str: string) {
+//   console.log(str.split("").reverse().join())
+// }
+
+// gerReverse("hello")
