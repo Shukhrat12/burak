41b993211b5bb9669051a1a62af1ed8704de4d42
Shukhfayz <shukhfayz@gmail.com>
2024-02-24 23:16:45 -0500

    feat: integration of environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-// console.log("executed")
+import dotenv from 'dotenv';
 
-// import moment from 'moment';
-
-// const currentTime = moment().format("YYYY MM DD")
-// console.log(currentTime)
-
-// const person: string = "Shawn";
-// const count: number = 100;
-
-// Architectural pattern: MVC, Dependency Injection, MVP
-// Architecture pattern - backendni suyagi, backenddagi ma'lumotlar oqimini tartibga soluvchi bir vosita
-
-// Design pattern - ma'lum bir bo'laklarini strukturasini yechishda xizmat qiladi
-// Design pattern: Middleware, Decorator
+dotenv.config();
