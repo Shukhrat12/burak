2e4914d9d07e67dfa053293db69119528b006172
Shukhfayz <shukhfayz@gmail.com>
2024-03-01 21:08:56 -0500

    feat: create project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
+        console.log('goHome')
         res.send("Home page")
     } catch (error) {
         console.log("Error, goHome", error)
@@ -12,6 +13,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
+        console.log('getLogin')
         res.send('Login page')
     } catch (error) {
         console.log("Error, getLogin", error)
@@ -20,6 +22,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
+        console.log('getSignup')
         res.send('Signup page')
     } catch (error) {
         console.log("Error, getSignup", error)
--- src/train.ts
+++ src/train.ts
@@ -1,31 +1,10 @@
-// H-TASK: 
-
-// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
-// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
-
-//with for loop
-// function getPositive(array: Array<number>) {
-//     let res="";
-//     for (let index = 0; index < array.length; index++) {
-
-//         const element = array[index];
-
-//         if (element>0) {
-           
-//             res += String(element);
-//         }
-       
-//     }
-//     return res
-// }
-
-// const res = getPositive([1, -4, 2]);
-// console.log(res)
-
-// with array iteration methods
-function getPositive(array: Array<number>) {
-   return array.filter((ele) => ele > 0 ? ele : null ).join("")
-}
-
-const res = getPositive([1, -4, 2]);
-console.log(res)
+/**
+ * Project Standards:
+ * - Logging standards
+ * - Naming standards:
+ *     functions, methods, variables = CAMEL
+ *     class => PASCAL
+ *     folder => KEBAB
+ *     css => SNAKE
+ * - Error handling
+ */
