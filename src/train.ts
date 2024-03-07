5e6443246dcaa115524a37e0eee66bf179163f6b
Shukhfayz <shukhfayz@gmail.com>
2024-03-06 23:58:01 -0500

    fix: I TASK

--- src/train.ts
+++ src/train.ts
@@ -1,10 +1,19 @@
-/**
- * Project Standards:
- * - Logging standards
- * - Naming standards:
- *     functions, methods, variables = CAMEL
- *     class => PASCAL
- *     folder, file => KEBAB
- *     css => SNAKE
- * - Error handling
- */
+// I-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+// @MITASK
+// for loop orqali
+function getDigits(str: string) {
+  let res="";
+  for (let i = 0; i < str.length; i++) {
+    if(str.charCodeAt(i) > 48 && str.charCodeAt(i) < 57) {
+      res += str[i]
+    }
+  }
+  return res;
+}
+
+const result = getDigits("m14i1t")
+console.log(result)
