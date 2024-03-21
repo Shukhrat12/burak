aed384eed0d2b8c15de2fbe79df5d990604589a4
Shukhfayz <shukhfayz@gmail.com>
2024-03-21 00:20:35 -0400

    feat: develop admin signup frontend js file

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,33 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let fileName;
+
+    fileTarget.on("change", function () {
+        if(window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            console.log("uploadFile: ", uploadFile)
+            const fileType = uploadFile["type"]
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"]
+
+            if(!validImageType.includes(fileType)){
+                alert("Please insert only jpeg, jpg, and png!")
+            } else {
+                if(uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile))
+                    $(".upload-img-frame").attr("src", URL.createObjectURL(uploadFile)).addClass("success");
+                }
+                fileName = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(fileName)
+        }
+    })
+
+
+
+
+});
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -15,5 +42,11 @@ function validateSignupForm() {
     if (memberPassword !== confirmPassword) {
         alert("Password doesn't match, please check!");
         return false;
+    }
+
+    const memberImage = $(".member-image").get(0).files[0].name ? $(".member-image").get(0).files[0].name : null;
+    if(!memberImage) {
+        alert("Please insert restaurant image!");
+        return false
     }
 }
