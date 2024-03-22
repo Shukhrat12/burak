d3daf7d07a10e8afa2b391118fcec277e618eb65
Shukhfayz <shukhfayz@gmail.com>
2024-03-22 17:43:09 -0400

    fix: modify validateSignupForm method in signupjs

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,49 +1,56 @@
 console.log("Signup frontend javascript file");
 
 $(function () {
-    const fileTarget = $(".file-box .upload-hidden");
-    let fileName;
+  const fileTarget = $(".file-box .upload-hidden");
+  let fileName;
 
-    fileTarget.on("change", function () {
-        if(window.FileReader) {
-            const uploadFile = $(this)[0].files[0];
-            console.log("uploadFile: ", uploadFile)
-            const fileType = uploadFile["type"]
-            const validImageType = ["image/jpg", "image/jpeg", "image/png"]
-
-            if(!validImageType.includes(fileType)){
-                alert("Please insert only jpeg, jpg, and png!")
-            } else {
-                if(uploadFile) {
-                    console.log(URL.createObjectURL(uploadFile))
-                    $(".upload-img-frame").attr("src", URL.createObjectURL(uploadFile)).addClass("success");
-                }
-                fileName = $(this)[0].files[0].name;
-            }
-            $(this).siblings(".upload-name").val(fileName)
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg, and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
         }
-    })
+        fileName = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(fileName);
+    }
+  });
 });
 
 function validateSignupForm() {
-    const memberNick = $(".member-nick").val();
-    const memberPhone = $(".member-phone").val();
-    const memberPassword = $(".member-password").val();
-    const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
 
-    if (memberNick === "" || memberPhone === "" || memberPassword === "" || confirmPassword === "") {
-        alert("Please insert all required inputs");
-        return false;
-    }
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please insert all required inputs");
+    return false;
+  }
 
-    if (memberPassword !== confirmPassword) {
-        alert("Password doesn't match, please check!");
-        return false;
-    }
+  if (memberPassword !== confirmPassword) {
+    alert("Password doesn't match, please check!");
+    return false;
+  }
 
-    const memberImage = $(".member-image").get(0).files[0].name ? $(".member-image").get(0).files[0].name : null;
-    if(!memberImage) {
-        alert("Please insert restaurant image!");
-        return false
-    }
+  const files = $(".member-image").get(0).files;
+  const memberImage = files.length > 0 ? files[0].name : null;
+
+  if (!memberImage) {
+    alert("Please insert a restaurant image!");
+    return false;
+  }
 }
