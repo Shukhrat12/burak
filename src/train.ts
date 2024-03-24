339c5027483b8af8c0d1322efdae2d33af97664c
Shukhfayz <shukhfayz@gmail.com>
2024-03-24 14:17:13 -0400

    fix: MITASK Q-TASK

--- src/train.ts
+++ src/train.ts
@@ -1,13 +1,14 @@
-// P-TASK:
+// Q-TASK:
 
-// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin.
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+// Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+// MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
 
 // @MITASK
 
-function objectToArray(obj: any) {
-  return Object.entries(obj)
+function hasProperty(obj: Object, str: string) {
+  const keys = Object.keys(obj)
+  return (keys.includes(str)) 
 }
 
-const result = objectToArray({a: 10, b: 20});
+const result = hasProperty({name: "BMW", model: "M3"}, "model")
 console.log(result)
